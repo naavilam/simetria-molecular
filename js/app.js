87083222044eeedbabe689f26f57e7e3edb3242c
Nara Avila <naraavilamoraes@gmail.com>
2025-06-13 23:48:27 -0300

    Update files

--- js/app.js
+++ js/app.js
@@ -11,28 +11,6 @@ const baseUrlGrupos = 'http://localhost:8000/api/grupo/';
 const baseUrlMoleculas = 'http://localhost:8000/api/molecula/';
 
 /******************************/
-/* AÇÃO SELECT GROUP          */
-/******************************/
-grupoSelect.addEventListener('change', () => {
-    const grupoSelecionado = grupoSelect.value;
-    if (grupoSelecionado === 'outro') {
-        grupoOutput.readOnly = false;
-        grupoOutput.value = "";
-    } else {
-        if (grupoSelecionado) {
-            fetch(baseUrlGrupos + grupoSelecionado)
-            .then(response => response.ok ? response.json() : Promise.reject('Erro ao carregar o JSON'))
-            .then(data => grupoOutput.value = JSON.stringify(data, null, 2))
-            .catch(err => grupoOutput.value = err);
-            grupoOutput.readOnly = true;   
-        } else {
-            grupoOutput.value = '';
-        }
-    }
-});
-
-
-/******************************/
 /* AÇÃO SELECT MOLECULE*/
 /******************************/
 moleculaSelect.addEventListener('change', () => {
@@ -70,21 +48,14 @@ analiseBtn.addEventListener("click", async () => {
   formData.append("molecula", moleculaBlob, "molecula.xyz");
 
     let paleta = null;
-    let operacao_id = null;
     const analises = {};
     let formato = null;
 
 
   // Apenas se renderização for gráfica
   if (tipo === "grafico") {
-    const grupoText = document.getElementById("grupoOutput")?.value ?? "";
-    const grupoBlob = new Blob([grupoText], { type: "application/json" });
-    paleta = document.querySelector('input[name="paletaCores"]:checked')?.value ?? null;
-    operacao_id = document.getElementById("select-operacao-unica")?.value ?? null;
-    formData.append("grupo", grupoBlob, "grupo.json");
-
-    const renderGrafico = document.querySelector('input[name="renderizacaoGrafica"]:checked')?.value ?? "";
-    formData.append("render_grafico", renderGrafico);
+    paleta = document.querySelector('input[name="paletaCores"]:checked')?.value ?? "PASTEL";
+    formato = document.querySelector('input[name="formatoGrafico"]:checked')?.value ?? "3d";
     }
   else {
     formato = document.querySelector(`input[name="formatoTexto"]:checked`)?.value ?? "";
@@ -98,8 +69,7 @@ try {
       render: {
         tipo,
         formato,
-        paleta,
-        operacao_id: operacao_id !== null ? parseInt(operacao_id) : null
+        paleta
       },
       analises
     };
@@ -144,64 +114,5 @@ function trocarRender() {
 }
 }
 
-// Só executa quando o usuário altera o grupo
-document.addEventListener("DOMContentLoaded", () => {
-  const grupoSelect = document.getElementById("grupoSelect");
-  if (!grupoSelect) return;
-
-  grupoSelect.addEventListener("change", () => {
-    carregarOperacoesDoGrupo();
-});
-});
-
-function carregarOperacoesDoGrupo() {
-    const containerSelect = document.getElementById("select-operacoes-container");
-    const grupoOutput = document.getElementById("grupoOutput");
-  containerSelect.innerHTML = ""; // limpa antes
-
-  try {
-    const grupoJson = grupoOutput.value.trim();
-
-    if (!grupoJson) {
-      throw new Error("Campo JSON está vazio");
-  }
-
-  const grupo = JSON.parse(grupoJson);
-
-  if (!Array.isArray(grupo.operacoes)) {
-      throw new Error("JSON inválido: campo 'operacoes' ausente ou incorreto");
-  }
-
-  const select = document.createElement("select");
-  select.id = "select-operacao-unica";
-
-  grupo.operacoes.forEach(op => {
-      const option = document.createElement("option");
-      option.value = op.id ?? "";
-      option.textContent = op.comentario || op.nome || `Op ${op.id ?? "?"}`;
-      select.appendChild(option);
-  });
-
-  containerSelect.innerHTML = "<h3>Operação a ser Renderizada:</h3>";
-  containerSelect.appendChild(select);
-
-} catch (err) {
-    console.warn("Erro ao carregar operações:", err.message);
-    containerSelect.innerHTML = `<p style='color:red'>Erro ao carregar operações. Verifique o JSON do grupo.</p>`;
-}
-}
-
 // Executa na inicialização da página
-document.addEventListener("DOMContentLoaded", trocarRender);
-
-// Excluir depois
-function injetarExemplo() {
-
-    const grupoOutput = document.getElementById("grupoOutput");
-    grupoOutput.readOnly = false;
-    grupoOutput.value = JSON.stringify(exemplo, null, 2);
-    grupoOutput.readOnly = true;
-
-    alert("Grupo D3h injetado com sucesso!");
-}
 document.addEventListener("DOMContentLoaded", trocarRender);
